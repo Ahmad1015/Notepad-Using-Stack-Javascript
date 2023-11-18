6527ce5bf1896272b9315e4727940b262019b062
Ahmad <mahmedraza90@gmail.com>
2023-11-18 18:17:15 +0500

    Menu Completed

--- notepad/src/App.js
+++ notepad/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Menu from './components/Menu';
 
+
 function App() {
   return (
     <>
--- notepad/src/components/Menu.js
+++ notepad/src/components/Menu.js
@@ -6,23 +6,21 @@ export default function menu() {
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
+    <a className="navbar-brand" href="/">Notepad</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="/">Home</a>
-        <a className="nav-link" href="/">Features</a>
-        <a className="nav-link" href="/">Pricing</a>
-        <a className="nav-link disabled" aria-disabled="true" href="/">Disabled</a>
+      </button>
+      <div className = "Buttons">
+      <button type="button" class="btn btn-success me-2">Save</button>
+      <button type="button" class="btn btn-primary">History</button>
+
       </div>
     </div>
-  </div>
-</nav>
+    </nav>
     </>
   )
 }
+
 
 
 
